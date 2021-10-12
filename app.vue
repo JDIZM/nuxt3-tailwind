a64f6ce885253ed8d88973a5299cc42f48a2f648
JDIZM <4444684+JDIZM@users.noreply.github.com>
2021-10-12 19:29:18 +0100

    :tada: nuxt 3 with tailwind

---
+++ app.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+import './assets/tailwind.css'
+</script>
+
+<template>
+   <NuxtPage />
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // mode: "jit",
+  // purge: [
+  //   "./components/**/*.{vue,js}",
+  //   "./layouts/**/*.vue",
+  //   "./pages/**/*.vue",
+  //   "./plugins/**/*.{js,ts}",
+  //   "./nuxt.config.{js,ts}",
+  // ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
