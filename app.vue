bc5357d2e929d4e8d61b1e6a18a88cf7840b88d5
JDIZM <4444684+JDIZM@users.noreply.github.com>
2021-10-13 21:33:05 +0100

    global style

--- app.vue
+++ app.vue
@@ -3,5 +3,13 @@ import './assets/tailwind.css'
 </script>
 
 <template>
+<div class="container flex p-4 justify-center">
    <NuxtPage />
+</div>
 </template>
+
+<style>
+p, div {
+  @apply mb-4;
+}
+</style>
