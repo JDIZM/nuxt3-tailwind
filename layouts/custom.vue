55fc6efc969500931c7f9ffaca55d11a5a215e3c
JDIZM <4444684+JDIZM@users.noreply.github.com>
2021-10-13 13:53:42 +0100

    custom layout

---
+++ layouts/custom.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLayout name="custom">
+    <template #header> Some header template content. </template>
+
+    The rest of the page
+  </NuxtLayout>
+</template>
+
+<script>
+export default {
+  layout: false,
+};
+</script>
