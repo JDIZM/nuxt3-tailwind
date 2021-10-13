864c2a97b9ad44af7562020365ef34ee08d02efa
JDIZM <4444684+JDIZM@users.noreply.github.com>
2021-10-13 19:34:49 +0100

    fixed: rm src - tailwind works in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // mode: "jit",
   purge: [
-    "./src/components/**/*.{vue,js}",
-    "./src/layouts/**/*.vue",
-    "./src/pages/**/*.vue",
-    "./src/plugins/**/*.{js,ts}",
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
   ],
   darkMode: false, // or 'media' or 'class'
