260b43895913a02e28cb075525e9a4663d8003e6
JDIZM <4444684+JDIZM@users.noreply.github.com>
2021-10-13 21:33:55 +0100

    updated index

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,22 @@
 <script lang="ts" setup>
 import HelloWorld from '@/components/HelloWorld.vue'
-const title = "hello world"
+const title = "Hello World"
+// fetching data from /server/api/hello.ts
 const data = await useFetch('/api/hello')
-console.log('home', data)
+const hello = data.data
 </script>
 
 <template>
   <div>
-    <HelloWorld :msg="title" />
-    <ul>
-      <li><nuxt-link to="/poke">blog</nuxt-link></li>
-    </ul>
+    <HelloWorld :msg="title" class="mb-4"/>
+    <div class="text-lg">
+      <nuxt-link to="/pokemon">Pokemon</nuxt-link> / 
+      <nuxt-link to="/poke">broken link</nuxt-link>
+    </div>
+    <div>
+      <p>fetching some data from /server/api/hello.ts</p>
+      <pre>{{ hello }}</pre>
+      <p>you can visit the api endpoint at <a href="/api/hello">/api/hello</a></p>
+    </div>
   </div>
 </template>
