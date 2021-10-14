99bb5e7b97d0ecef3839cf20b1d809b5b6974424
JDIZM <4444684+JDIZM@users.noreply.github.com>
2021-10-14 21:17:12 +0100

    world api end point

---
+++ server/api/world.ts
@@ -0,0 +1,22 @@
+import type { IncomingMessage, ServerResponse } from 'http';
+
+// make a request with query params to /api/world?key=value
+export default async (req: IncomingMessage, res: ServerResponse) => {
+    const url = new URL(req.url, `http://${req.headers.host}/api/world`);
+    const obj = {};
+
+    console.log('api req', url);
+
+    for (const [key, value] of url.searchParams) {
+        obj[key] = value;
+    };
+
+    if (!Object.entries(obj).length) {
+        res.statusCode = 404;
+        console.log('api res', res.statusCode);
+        return res.end('nothing found');
+    }
+
+    console.log('api res', res.statusCode);
+    res.end(JSON.stringify(obj));
+  }
