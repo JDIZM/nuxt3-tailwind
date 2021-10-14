d7b11163fbd9ee6bd9983ee4ab018dcc61e0d28d
JDIZM <4444684+JDIZM@users.noreply.github.com>
2021-10-14 21:16:19 +0100

    log middleware

--- server/middleware/middleware.ts
+++ server/middleware/middleware.ts
@@ -3,6 +3,6 @@ import type { IncomingMessage, ServerResponse } from 'http'
 export default async (req: IncomingMessage, res: ServerResponse) => {
     // middleware intercepts request and response
     // can modify and return new values based on conditions
-    console.log('req', req.url)
-    console.log('res', res.statusCode, res.statusMessage)
+    console.log('middleware req', req.url)
+    console.log('middleware res', res.statusCode)
   }
